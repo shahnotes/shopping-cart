70fb0d580a794b0e195b0108d20202c646c300cf
Harsh Shah <shahharsh.210995@gmail.com>
2025-07-19 23:08:50 +0530

    Essentials

---
+++ config/essentials.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    NunoMaduro\Essentials\Configurables\ShouldBeStrict::class,
+    NunoMaduro\Essentials\Configurables\Unguard::class,
+    NunoMaduro\Essentials\Configurables\ProhibitDestructiveCommands::class,
+    NunoMaduro\Essentials\Configurables\ImmutableDates::class,
+];
